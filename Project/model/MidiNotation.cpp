5e844817617382cf176df01b635bbac373958cea
hewei <378989081@qq.com>
2017-07-25 17:48:41 +0800

    音符model

---
+++ Project/model/MidiNotation.cpp
@@ -0,0 +1,15 @@
+﻿#include "MidiNotation.h"
+
+int MidiNotation::getDynamics() 
+{
+	return _nDynamic;
+}
+
+int MidiNotation::getMusicaScale()
+{
+	return _nMusicalScale;
+}
+
+BYTE MidiNotation::getPitch() {
+	return _ePitch;
+}
